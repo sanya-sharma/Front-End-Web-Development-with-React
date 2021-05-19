08cca85811807280d870ca4e07becde15100593a
Sanya Sharma <sanya.sharma2511@gmail.com>
2021-05-19 09:43:28 +0530

    Fetch form Server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,6 +3,8 @@ import {Card, CardImg, CardBody, CardText, CardTitle, Breadcrumb, BreadcrumbItem
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent.js';
+import { baseUrl } from '../shared/baseUrl.js';
+
 
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
@@ -110,7 +112,7 @@ function RenderDish({dish}) {
         return(
             <div className="col-12 col-md-5 m-1">
                 <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name}/>
+                    <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name}/>
                     <CardBody>
                         <CardTitle>{dish.name}</CardTitle>
                         <CardText>{dish.description}</CardText>
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
 import { Loading } from './LoadingComponent.js';
+import { baseUrl } from '../shared/baseUrl.js';
+
 
 function RenderCard({item, isLoading, errMess}) {
 
@@ -17,7 +19,7 @@ function RenderCard({item, isLoading, errMess}) {
     else{
         return(
             <Card>
-                <CardImg src={item.image} alt={item.name} />
+                <CardImg src={baseUrl + item.image} alt={item.name} />
                 <CardBody>
                     <CardTitle>{item.name}</CardTitle>
                     {item.designation ? <CardSubtitle >{item.designation}</CardSubtitle> : null}
@@ -39,7 +41,9 @@ function Home(props) {
                     errMess={props.dishesErrMess} />
             </div>
             <div className="col-12 col-md m-1">
-                <RenderCard item={props.promotion} />
+                <RenderCard item={props.promotion}
+                isLoading={props.promosLoading}
+                errMess={props.promosErrMess}  />
             </div>
             <div className="col-12 col-md m-1">
                 <RenderCard item={props.leader} />
